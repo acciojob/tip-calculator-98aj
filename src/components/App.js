d1fc106cca347c9c7186abcdd268134a7d5fc8b2
98aj <103376356+98aj@users.noreply.github.com>
2023-06-15 21:25:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,10 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
   return (
     <div>
-        <div><lable>Bill : </lable> <input/></div>
+        <div><lable>Bill : </lable> <input id='billamt'/></div>
         <div><lable>Tip Percentage : </lable> <input/></div>
         <div><lable>Number of People : </lable> <input/></div>
     </div>
