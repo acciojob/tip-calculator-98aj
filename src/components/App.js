3e8897a17a7b4bcb20effbf11e64c773d7f4360b
98aj <103376356+98aj@users.noreply.github.com>
2023-06-15 21:31:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
         <div><lable>Bill : </lable> <input id='billamt' type='number'/></div>
-        <div><lable>Tip Percentage : </lable> <input type='number'/></div>
+        <div><lable>Tip Percentage : </lable> <input id='serviceQual' type='number'/></div>
         <div><lable>Number of People : </lable> <input type='number'/></div>
     </div>
   )
